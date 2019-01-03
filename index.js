4d6f4575efd37c9baf40e4c92c81db83f47daec2
Gabriel Freire <gabrielfvale@gmail.com>
2019-01-03 17:57:19 -0300

    Support for detailed RU menu

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const config = {
 const x = Xray({
     filters: {
         trim: value => value.trim(),
-        fulltrim: value => value.trim().replace(/\n/g, '').replace(/\t/g, '')
+        fulltrim: value => value.trim().replace(/\n/g, '').replace(/\t/g, ''),
+        cardapio_empty: value => value === 'Bebidas' ? false : true
     }
 });
 
@@ -48,6 +49,37 @@ const creditscrape = (html) => {
             detalhes: 'td:nth-child(3) | fulltrim'
         }])
     });
+}
+
+const cardapioscrape = (data) => {
+    const url = 'http://www.ufc.br/restaurante/cardapio/1-restaurante-universitario-de-fortaleza/' + data;
+    return x(url, {
+        empty: '.c-cardapios table td | cardapio_empty',
+        cafe: {
+            bebidas: ['td.bebidas span[class="desc"]'],
+            paes: ['td.paes span[class="desc"]'],
+            frutas: ['td.frutas span[class="desc"]'],
+            especial: ['td.especial span[class="desc"]'],
+        },
+        almoco: {
+            principal: ['table.almoco td.principal span[class="desc"]'],
+            vegetariano: 'table.almoco td.vegetariano span[class="desc"]',
+            salada: 'table.almoco td.salada span[class="desc"]',
+            guarnicao: 'table.almoco td.guarnicao span[class="desc"]',
+            acmp: ['table.almoco td.acompanhamento span[class="desc"]'],
+            suco: 'table.almoco td.suco span[class="desc"]',
+            sobremesa: ['table.almoco td.sobremesa span[class="desc"]'],
+        },
+        jantar: {
+            principal: ['table.jantar td.principal span[class="desc"]'],
+            vegetariano: 'table.jantar td.vegetariano span[class="desc"]',
+            salada: 'table.jantar td.salada span[class="desc"]',
+            guarnicao: 'table.jantar td.guarnicao span[class="desc"]',
+            acmp: ['table.jantar td.acompanhamento span[class="desc"]'],
+            suco: 'table.jantar td.suco span[class="desc"]',
+            sobremesa: ['table.jantar td.sobremesa span[class="desc"]'],
+        },
+    })
 }
 
 app.get('/', (req, res) => {
@@ -90,6 +122,12 @@ app.get('/creditos', (req, res) => {
         }
     });
 });
+
+app.get('/cardapio/:data?', (req, res) => {
+    const data = req.params.data;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    cardapioscrape(data).write().pipe(res);
+})
 
 MongoClient.connect(config.mongoURI, { useNewUrlParser: true }, (err, client) => {
     db = client.db('sigaapi');
